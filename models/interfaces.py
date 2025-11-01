7f7ff61348ab4fc9353d713b123e3b0501a9ec3b
kirmorozov1992 <kirmorozov1992@yandex.ru>
2025-11-02 00:59:29 +0600

    add abstract classes MetadataProcessor, Creator, Snapshotter, Loader

---
+++ models/interfaces.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+class MetadataProcessor(ABC):
+    @abstractmethod
+    def get_datetime_job_run(self, *, conn: Any):
+        pass
+
+    @abstractmethod
+    def update_datetime_job_run(self, *, conn: Any, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_current_date(self, *, conn: Any):
+        pass
+
+    @abstractmethod
+    def get_column_names(self, *, conn: Any):
+        pass
+
+    @abstractmethod
+    def get_clause(self, *, conn: Any, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_job_meta(self, *, conn: Any, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_id_col_data_type(self, *, conn: Any, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_log_count(self, *, conn: Any, **kwargs):
+        pass
+
+    @abstractmethod
+    def set_log_count(self, *, conn: Any):
+        pass
+
+    @abstractmethod
+    def make_day_offset(self, *, conn: Any, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_min_max_value(self, *, conn: Any, **kwargs):
+        pass
+    
+
+class Creator(ABC):
+    @abstractmethod
+    def create_table(self, *, conn: Any, **kwargs):
+        pass
+
+class Snapshotter(ABC):
+    @abstractmethod
+    def snapshot(self, *, conn: Any):
+        pass
+
+class Loader(ABC):
+    @abstractmethod
+    def collect_data(self, *, conn: Any, **kwargs):
+        pass
+
+    @abstractmethod
+    def insert(self, *, conn: Any, **kwargs):
+        pass
+
+    @abstractmethod
+    def upsert(self, *, conn: Any, **kwargs):
+        pass
+
+    @abstractmethod
+    def process_data(self, *, conn: Any, **kwargs):
+        pass
+
